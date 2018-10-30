f85542ede0111ce430c74a98babb8c0b4f83ede6
Avneetkaur Matharu <avneetmatharu64@gmail.com>
2018-10-30 14:45:00 -0400

    Master page modified

--- SoccerLeague/MasterPage.Master.designer.cs
+++ SoccerLeague/MasterPage.Master.designer.cs
@@ -31,6 +31,15 @@ namespace SoccerLeague {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
