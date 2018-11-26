612e66bc42a24c6129555fc6bbc77199064b30f6
sai91603 <saicharanmittapalli@gmail.com>
2018-11-26 15:36:04 -0500

    Added content in clubs.aspx.cs file

--- SoccerLeague/Clubs.aspx.cs
+++ SoccerLeague/Clubs.aspx.cs
@@ -9,9 +9,39 @@ namespace SoccerLeague
 {
     public partial class Clubs : System.Web.UI.Page
     {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Session["Theme"] != null)
+            {
+                this.Theme = Session["Theme"].ToString();
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            DisplayClubsList();
+        }
+
+        private void DisplayClubsList()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("psDisplayClub", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                connection.Open();
+
+                GridView1.DataSource = command.ExecuteReader();
+                GridView1.DataBind();
+
+            }
 
         }
+
+        protected void LinkButtonClick(Object sender, EventArgs e)
+        {
+            Session["ClubName"] = ((LinkButton)sender).Text;
+            Response.Redirect("~/Pages/ClubDetails.aspx");
+        }
     }
-}
