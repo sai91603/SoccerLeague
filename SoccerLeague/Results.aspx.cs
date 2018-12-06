b30dc83812bfe19ec13a343e4edea4d0f116ba20
Sukhpreetsandhu <sukh95sandhu@gmail.com>
2018-12-06 12:23:23 -0500

    Added content in results.aspx.cs page

--- SoccerLeague/Results.aspx.cs
+++ SoccerLeague/Results.aspx.cs
@@ -9,9 +9,31 @@ namespace SoccerLeague
 {
     public partial class Results : System.Web.UI.Page
     {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Session["Theme"] != null)
+            {
+                this.Theme = Session["Theme"].ToString();
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand("spDisplayResults", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    connection.Open();
+
+                    gridMatchResult.DataSource = command.ExecuteReader();
+                    gridMatchResult.DataBind();
+
+                }
+            }
         }
     }
 }
